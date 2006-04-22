7eee4792938ebe28e4cd5f47e363e94e87d69980
Jim Fulton <jim@zope.com>
2006-04-22 18:53:22 +0000

    Changed the way modules are moved:
    - Removed the deprecatedModule API.  It didn't really work.  It
      created a proxies for deprecated modules in sys.modules that
      generated warnings when names were accessed.  Unfortunately, there
      many analysis systems that inspect all modules found sys.modules.
      When one os these hit a deprecated module, it generated spurious
      deprecation warnings
    
    - Added a new API to zope.deprecation to be used on placeholders for
      moved modules in their old locations.  With this approach, moved
      modules only generate warnings when they are actually imported.
    
    - Added placeholders for various modiles moved out of zope.app.
      When we are ready to really get rid of these, we'll just remove the
      placeholders.

--- __init__.py
+++ __init__.py
@@ -1,4 +1,3 @@
 from zope.deferredimport.deferredmodule import initialize
 from zope.deferredimport.deferredmodule import define, defineFrom
 from zope.deferredimport.deferredmodule import deprecated, deprecatedFrom
-from zope.deferredimport.deferredmodule import deprecatedModule
