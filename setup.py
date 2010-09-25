cb3e0fee74ac97e94adbd1a10f71af692e56f29f
Michael Howitz <mh@gocept.com>
2010-09-25 12:47:42 +0000

    Preparing release 3.5.3

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ def read(*rnames):
 
 setup(
     name='zope.deferredimport',
-    version = '3.5.3dev',
+    version='3.5.3',
     url='http://pypi.python.org/pypi/zope.deferredimport',
     license='ZPL 2.1',
     description='zope.deferredimport allows you to perform imports names '
