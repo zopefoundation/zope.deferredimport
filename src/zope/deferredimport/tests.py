4023a69f27a2ea42cc077dde3a4ebf92604d73ac
Tres Seaver <tseaver@palladion.com>
2015-02-03 10:41:30 -0500

    Progress toward 100% coverage.
    See #1.

--- src/zope/deferredimport/tests.py
+++ src/zope/deferredimport/tests.py
@@ -14,7 +14,37 @@
 import unittest
 
 
+class DeferredTests(unittest.TestCase):
+
+    def _getTargetClass(self):
+        from zope.deferredimport.deferredmodule import Deferred
+        return Deferred
+
+    def _makeOne(self, *args, **kw):
+        return self._getTargetClass()(*args, **kw)
+
+    def test_ctor(self):
+        name = 'TESTING'
+        specifier = 'foo:bar'
+        deferred = self._makeOne(name, specifier)
+        self.assertEqual(deferred.__name__, name)
+        self.assertEqual(deferred.specifier, specifier)
+
+    def test_get_just_module(self):
+        name = 'TESTING'
+        specifier = 'zope.deferredimport.deferredmodule'
+        deferred = self._makeOne(name, specifier)
+        from zope.deferredimport import deferredmodule
+        self.assertTrue(deferred.get() is deferredmodule)
+
+    def test_get_module_and_name(self):
+        name = 'TESTING'
+        specifier = 'zope.deferredimport.deferredmodule:Deferred'
+        deferred = self._makeOne(name, specifier)
+        self.assertTrue(deferred.get() is self._getTargetClass())
+
+
 def test_suite():
     return unittest.TestSuite((
-        # TODO:  replace removed doctests
-        ))
+        unittest.makeSuite(DeferredTests),
+    ))
