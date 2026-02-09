68f7d17c74bfc865ef548380baf5ef0fd3032be6
Michael Howitz <icemac@gmx.net>
2026-02-09 09:07:03 +0100

    Move package metadata from setup.py to pyproject.toml.

--- setup.py
+++ setup.py
@@ -16,73 +16,10 @@
 # When developing and releasing this package, please follow the documented
 # Zope Toolkit policies as described by this documentation.
 ##############################################################################
-"""Setup for zope.deferredimport package
-"""
-import os
+"""Setup for zope.deferredimport package"""
 
 from setuptools import setup
 
 
-def read(*rnames):
-    with open(os.path.join(os.path.dirname(__file__), *rnames)) as f:
-        return f.read()
-
-
-TESTS_REQUIRE = [
-    'zope.testrunner >= 6.4',
-]
-
-DOCS_REQUIRE = [
-    'Sphinx',
-    'repoze.sphinx.autointerface',
-]
-
-setup(
-    name='zope.deferredimport',
-    version='6.1.dev0',
-    url='http://github.com/zopefoundation/zope.deferredimport',
-    license='ZPL-2.1',
-    description=('zope.deferredimport allows you to perform imports names '
-                 'that will only be resolved when used in the code.'),
-    project_urls={
-        'Issue Tracker': ('https://github.com/zopefoundation/'
-                          'zope.deferredimport/issues'),
-        'Sources': 'https://github.com/zopefoundation/zope.deferredimport',
-    },
-
-    author='Zope Foundation and Contributors',
-    author_email='zope-dev@zope.dev',
-    long_description=(
-        read('README.rst')
-        + '\n\n' +
-        read('CHANGES.rst')
-    ),
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Zope Public License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-        'Programming Language :: Python :: 3.12',
-        'Programming Language :: Python :: 3.13',
-        'Programming Language :: Python :: 3.14',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
-        'Topic :: Internet :: WWW/HTTP',
-        'Topic :: Software Development',
-    ],
-    python_requires='>=3.10',
-    install_requires=[
-        'zope.proxy',
-    ],
-    extras_require={
-        'test': TESTS_REQUIRE,
-        'docs': DOCS_REQUIRE,
-    },
-    include_package_data=True,
-    zip_safe=False,
-)
+# See pyproject.toml for package metadata
+setup()
